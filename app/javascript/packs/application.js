f14371b71027f45f9952a2086b55491aa1924810
bilalbash <bilal.basharat@gmail.com>
2020-09-17 11:44:41 +0500

    basic bootstrap css and js is being setup

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,9 +3,11 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+import 'bootstrap'
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
+
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -14,3 +16,12 @@ require("@rails/activestorage").start()
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+console.log('im working');
+document.addEventListener('turbolinks:load', () => {
+  $('[data-toggle="tooltip"]').tooltip()
+  $('[data-toggle="popover"]').popover()
+  console.log('im working');
+})
+
+
